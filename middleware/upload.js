6833d9990b929a60bac790d8a5bc447f90c4769f
Souradeep Saha <sahasouradeep01@gmail.com>
2021-05-09 16:47:01 -0500

    Update path for image upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,9 @@
 const multer = require("multer");
+const path = require("path");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (req, file, cb) {
     const ext = file.originalname.substr(file.originalname.lastIndexOf("."));
